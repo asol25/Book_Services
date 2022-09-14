7b32c4a9f43b1ee2719b8da0ad107954162fdf7a
asol25 <usool.203@gmail.com>
2022-09-14 14:08:17 +0700

    config require_one

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 require_once 'vendor/autoload.php';
-require_once 'core/application.php';
-include_once 'core/Application.php';
+require_once 'core/Application.php';
 use app\core\Application;
 use app\src\controllers\CallbackController;
 use app\src\controllers\HomeController;
