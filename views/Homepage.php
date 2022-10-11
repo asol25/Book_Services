558afea9f97127da1612496eb0a1cf69ad4d2241
asol25 <usool.203@gmail.com>
2022-10-12 00:06:26 +0700

    fix UI layout

--- views/Homepage.php
+++ views/Homepage.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'views/components/most_views.php';
+include_once 'views/components/most_views.php';
 ?>
 
 <div class="main_products_container">
     <?php
-    require 'views/components/category_views.php';
-    require 'views/components/products_views.php';
+    include_once 'views/components/category_views.php';
+    include_once 'views/components/products_views.php';
     ?>
 </div>
