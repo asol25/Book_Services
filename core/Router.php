7fe5af45d9a4ca4a4d1653a18da4587e455e96aa
asol25 <usool.203@gmail.com>
2022-10-11 23:48:04 +0700

    fix Route

--- core/Router.php
+++ core/Router.php
@@ -86,6 +86,7 @@ class Router
     {
         ob_start();
         include_once "views/main/index.php";
+        var_dump(include_once "views/main/index.php");
         return ob_get_clean();
     }
 
